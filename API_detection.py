44ffedf8f61e18dd593de343d6817383314e49bf
Bend_Function <42468146+Bend-Function@users.noreply.github.com>
2018-11-11 11:53:54 +0800

    Update API_detection.py

--- API_detection.py
+++ API_detection.py
@@ -1,4 +1,7 @@
-# 实现任务1.1
+# 请自行准备模型
+# 官方模型地址：https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
+# Bibili space：https://space.bilibili.com/275177832
+# 视频演示：https://www.bilibili.com/video/av35255202
 
 import numpy as np
 import os
